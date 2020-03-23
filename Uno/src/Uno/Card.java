cf6cec7c474b6004ea06fb9d1f4f80ed006c1267
админ <@DESKTOP-F7U54D6>
2020-03-23 13:42:46 +0100

    No commit message

---
+++ Uno/src/Uno/Card.java
@@ -0,0 +1,11 @@
+package Uno;
+
+public class Card {
+	private int cardNumber;
+    private char cardColor;
+    
+    public Card(int cNumber, char cColor){
+        cardNumber = cNumber;
+        cardColor = cColor;
+    }
+}
